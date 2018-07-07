6eaa45f31ca0eb9d52e6f3ae791a05f463324ab3
Gabriel Valle <gvalle91@gmail.com>
2018-07-07 15:59:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,10 +17,10 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @counter = 0
-    while @counter < @number
-      "#{@phrase}"
-      @counter += 1
-    end
+    <% while @counter < @number %>
+      <%= "#{@phrase}" %>
+      <% @counter += 1 %>
+    <% end %>
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
